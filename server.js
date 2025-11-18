69cb3e4158c3dba10caa0b9b52687ce236c5f858
mayuu3 <marathemayu2005@gmail.com>
2025-11-18 23:01:23 +0530

    Replace bcrypt with bcryptjs for password hashing

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
+
 
 dotenv.config();
 
